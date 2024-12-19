c4178891ecfc54fe74ca6fe492871eb5987cbd05
nandhini-data <nandhini.abishek@outlook.com>
2024-12-19 16:08:54 +0800

    functions added

---
+++ 11.Functions/mul_add_list1.py
@@ -0,0 +1,17 @@
+
+def mult(number):
+    num=1
+    for i in number:
+        x=i
+        num=x*num
+    return num
+    
+def addition(number1):
+    num1=0
+    for n in number1:
+        y=n
+        num1=y+num1
+    return num1
+
+def greet(name1,name2):
+    return name1+ ""+name2
